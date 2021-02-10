bcafa0da1024092c81999b9f2f0a985c08338725
Hongggggggg <hongggggggghammer@gmail.com>
2021-02-10 17:25:08 +0800

    -多继承

--- Base_Knowledge/类/继承.py
+++ Base_Knowledge/类/继承.py
@@ -17,7 +17,7 @@ class Person(Animal):   #继承Animal
         self.sex = sex
         print('Sex is %s' % sex)
 
-    def speek(self):    #私有方法
+    def speak(self):    #私有方法
         print('Speaking')
 
 
@@ -32,9 +32,42 @@ class Pig(Animal):
 
 
 p1 = Person('Hammer', 25, 'M')
-p1.speek()
+p1.speak()
 print(p1.type)
 
 pig = Pig('pig', 2)
 print(pig.type)
 pig.walk()
+
+
+
+class Peiqi(Pig, Person):
+    def sayhi(self):
+        print("I am peiqi")
+
+class Shenxian:
+    def fly(self):
+        print("上天")
+
+    def fight(self):
+        print('神仙打架')
+
+
+class Monkey:
+    def dance(self):
+        print("跳舞")
+    def fight(self):
+        print('群魔乱舞')
+
+class Monkeysun(Shenxian, Monkey): #多继承
+    def change(self):
+        print('72变')
+
+    
+
+
+sunwukong = Monkeysun()
+sunwukong.change()
+sunwukong.fly()
+sunwukong.dance()
+sunwukong.fight() #先继承谁，就用的谁的方法
