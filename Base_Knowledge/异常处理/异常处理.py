3e69e9b024a82883699eacd61dac1ab3b8131d41
Hongggggggg <hongggggggghammer@gmail.com>
2021-02-26 23:56:33 +0800

    -自定义异常

--- Base_Knowledge/异常处理/异常处理.py
+++ Base_Knowledge/异常处理/异常处理.py
@@ -14,8 +14,32 @@ except AttributeError as e:
 
 
 try:
-    print(ooo)
+    print(1)
+    raise IndexError #手动触发异常
 except NameError as e:
     print(e)
+except Exception as e:
+    print('万能异常', e)
+
+else:
+    print('没发生异常')
+
+finally:
+    print('异常不异常都会走这')
 
 
+class WebConnErr(BaseException):#自定义异常
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+try:
+    print("Connect to Google")
+    raise WebConnErr('无法翻墙访问')
+except WebConnErr as e:
+    print(e)
+except Exception as e: #无法抓到自定义的异常
+    print(e)
